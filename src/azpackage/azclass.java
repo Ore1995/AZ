8ed527d272ef7d6ec18800663308a71873c04683
Ore1995 <chalarisorestis@gmail.com>
2021-03-05 14:50:54 +0200

    message3

--- src/azpackage/azclass.java
+++ src/azpackage/azclass.java
@@ -5,7 +5,8 @@ public class azclass {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World");
-		System.out.println("Hello World from Github");
+		System.out.println("Java is hard");
+		
 	}
 
 }
